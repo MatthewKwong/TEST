8bdc83ecdcda57b59799da1ec47cc110ce127ba5
MatthewKwong <mkwong.sh@mysummitps.org>
2016-06-17 23:39:51 +0000

    new

--- script.js
+++ script.js
@@ -1,8 +1,7 @@
           $(document).ready(function() {
-               $("#logo").effect( "bounce", {times:1}, 530 );
-            });
-
-
-
-// background is as big as the screen size
-// document.body.style.height = window.innerHeight;
+              $("#logo").effect("bounce", {
+                  times: 1
+              }, 530);
+          });
+           // background is as big as the screen size
+           // document.body.style.height = window.innerHeight;
